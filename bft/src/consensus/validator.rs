f4deaf4062987fc83f000ffc942c551af4f54ce9
Rg <daimaldd@gmail.com>
2018-10-09 01:19:13 +0800

    update

--- bft/src/consensus/validator.rs
+++ bft/src/consensus/validator.rs
@@ -1,6 +1,7 @@
 use cryptocurrency_kit::common::to_hex;
 use cryptocurrency_kit::ethkey::Address;
 
+use super::types::Height;
 use std::cmp::{Ord, Ordering, PartialEq};
 
 pub trait Validator: ::std::fmt::Debug + ::std::fmt::Display {
@@ -47,13 +48,54 @@ impl ::std::fmt::Display for ImplValidator {
 pub trait ValidatorSet: Clone {
     fn calc_proposer(last_proposer: Address, round: u64);
     fn size(&self) -> usize;
-    fn list(&self) -> Vec<Box<dyn Validator>>;
-    fn get_by_index(&self) -> Validator;
-    fn get_by_address(&self, address: Address) -> Validator;
+    fn list(&self) -> Vec<Box<Validator>>;
+    fn get_by_index<T: Validator>(&self) -> Option<T>;
+    fn get_by_address(&self, address: Address) -> Option<Box<Validator>>;
     // get current proposer
-    fn get_proposer(&self) -> Validator;
+    fn get_proposer(&self) -> &Validator;
     fn is_proposer(&self, address: Address) -> bool;
     fn add_validator(&mut self, address: Address) -> bool;
     fn remove_validator(&mut self, address: Address) -> bool;
     fn f() -> isize;
+}
+
+type ProposalSelector = Box<Fn()>;
+
+fn selectorFn() {}
+
+struct ImplValidatorSet<T: Validator>{
+    validators: Validators,
+    proposer: T,
+    selector: ProposalSelector,
+}
+
+
+impl<T: Validator> ImplValidatorSet<T> {
+//    fn new<T>(address: &[Address]) -> Self {
+//        let mut set = ImplValidatorSet{
+//            validators: Vec::new();
+//            proposer: Box::new(selectorFn),
+//        };
+//
+//        let mut validators:Vec<Address> = Vec::new();
+//        for x in address {
+//            validators.push(x.clone());
+//        }
+//
+//        // TODO sort address
+//
+//
+//    }
+
+    pub fn size(&self) -> usize {
+        self.validators.len()
+    }
+
+    pub fn list(&self) -> Box<Validators> {
+        let mut validators = Vec::new();
+        for validator in self.validators {
+            validators.push(validator);
+        }
+        Box::new(validators)
+    }
 }
