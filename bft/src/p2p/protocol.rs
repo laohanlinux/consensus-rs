ca25e350dc3416165d075f1d0e79fc6d530064f0
Rg <daimaldd@gmail.com>
2018-12-05 01:38:41 +0800

    :+1:

--- bft/src/p2p/protocol.rs
+++ bft/src/p2p/protocol.rs
@@ -1,12 +1,31 @@
 use std::borrow::Cow;
 use std::io::Cursor;
+use std::str::FromStr;
 
+use libp2p::{PeerId, Multiaddr};
 use cryptocurrency_kit::crypto::{CryptoHash, Hash, hash};
 use cryptocurrency_kit::storage::values::StorageValue;
 use rmps::decode::Error;
 use rmps::{Deserializer, Serializer};
 use serde::{Deserialize, Serialize};
 
+#[derive(Debug, Clone, Deserialize, Serialize, Message)]
+pub enum P2PMsgCode {
+    Handshake,
+    Transaction,
+    Block,
+    Consensus,
+    Sync,
+}
+
+implement_storagevalue_traits! {P2PMsgCode}
+implement_cryptohash_traits! {P2PMsgCode}
+
+#[derive(Debug, Clone, Copy)]
+pub enum BoundType {
+    InBound,
+    OutBound,
+}
 
 #[derive(Debug, Clone, Deserialize, Serialize, Message)]
 pub struct RawMessage {
@@ -18,37 +37,71 @@ implement_storagevalue_traits! {RawMessage}
 implement_cryptohash_traits! {RawMessage}
 
 impl RawMessage {
-    fn new(header: Header, payload: Vec<u8>) -> Self {
-        RawMessage{
+    pub fn new(header: Header, payload: Vec<u8>) -> Self {
+        RawMessage {
             header: header,
             payload: payload,
         }
     }
 
-    fn header(&self) -> &Header {
+    pub(crate) fn header(&self) -> &Header {
         &self.header
     }
 
-    fn mut_header(&mut self) -> &mut Header {
+    pub(crate) fn mut_header(&mut self) -> &mut Header {
         &mut self.header
     }
 
-    fn payload(&self) -> &Payload {
+    pub(crate) fn payload(&self) -> &Payload {
         &self.payload
     }
 
-    fn mut_payload(&mut self) -> &mut Payload {
+    pub(crate) fn mut_payload(&mut self) -> &mut Payload {
         &mut self.payload
     }
 }
 
 #[derive(Debug, Clone, Deserialize, Serialize)]
 pub struct Header {
-    ttl: usize,
-    create_time: u64,
+    pub code: P2PMsgCode,
+    pub ttl: usize,
+    pub create_time: u64,
 }
 
 implement_cryptohash_traits! {Header}
 implement_storagevalue_traits! {Header}
 
+impl Header {
+    pub fn new(code: P2PMsgCode, ttl: usize, create_time: u64) -> Self {
+        Header { code: code, ttl: ttl, create_time: create_time }
+    }
+}
+
 pub type Payload = Vec<u8>;
+
+#[derive(Debug, Clone, Deserialize, Serialize)]
+pub struct Handshake {
+    version: String,
+    peer_id: String,
+}
+
+implement_storagevalue_traits! {Handshake}
+implement_cryptohash_traits! {Handshake}
+
+impl Handshake {
+    pub fn new(version: String, peer_id: PeerId) -> Self {
+        let peer_id = peer_id.to_base58();
+        Handshake {
+            version: version,
+            peer_id: peer_id,
+        }
+    }
+
+    pub fn version(&self) -> &String {
+        &self.version
+    }
+
+    pub fn peer_id(&self) -> PeerId {
+        PeerId::from_str(&self.peer_id).unwrap()
+    }
+}
--- bft/src/p2p/server.rs
+++ bft/src/p2p/server.rs
@@ -1,9 +1,10 @@
 use std::collections::HashMap;
 use std::net;
 use std::str::FromStr;
-
+use std::time::Duration;
 use std::any::{Any, TypeId};
 
+use cryptocurrency_kit::storage::values::StorageValue;
 use tokio::{codec::FramedRead, io::AsyncRead, io::WriteHalf, net::TcpListener, net::TcpStream};
 use actix::prelude::*;
 use futures::prelude::*;
@@ -21,28 +22,63 @@ use libp2p::{
 };
 
 use crate::{
-    common::multiaddr_to_ipv4,
+    common::{random_uuid, multiaddr_to_ipv4},
     subscriber::P2PEvent,
     util::{TimerOp, TimerRuntime},
+    error::P2PError,
 };
 use super::codec::MsgPacketCodec;
 use super::session::{self, Session};
+use super::protocol::{RawMessage, P2PMsgCode, Handshake, BoundType};
 
 pub const MAX_OUTBOUND_CONNECTION_MAILBOX: usize = 1 << 10;
 pub const MAX_INBOUND_CONNECTION_MAILBOX: usize = 1 << 9;
 
-#[derive(Message)]
+lazy_static! {
+    pub static ref ZERO_PEER: PeerId = {
+        PeerId::from_str("QmX5e9hkQf7B45e2MZf38vhsC2wfA5aKQrrBuLujwaUBGw").unwrap()
+    };
+}
+
 pub enum ServerEvent {
-    Connected(PeerId),
+    Connected(PeerId, BoundType, RawMessage),
+    // handshake
     Disconnected(PeerId),
+    Message(RawMessage),
+}
+
+
+impl Message for ServerEvent {
+    type Result = Result<PeerId, P2PError>;
 }
 
 pub struct TcpServer {
     pid: Addr<TcpServer>,
     key: Option<secio::SecioKeyPair>,
     node_info: (PeerId, Multiaddr),
-    peers: HashMap<PeerId, u8>,
-    dial_peers: HashMap<PeerId, Uuid>,
+    peers: HashMap<PeerId, ConnectInfo>,
+}
+
+struct ConnectInfo {
+    connect_time: chrono::DateTime<chrono::Utc>,
+    bound_type: BoundType,
+}
+
+impl ConnectInfo {
+    fn new(connect_time: chrono::DateTime<chrono::Utc>, bound_type: BoundType) -> Self {
+        ConnectInfo {
+            connect_time: connect_time,
+            bound_type: bound_type,
+        }
+    }
+}
+
+fn node_info(peers: &HashMap<PeerId, ConnectInfo>) -> String {
+    let mut info: Vec<String> = vec![];
+    for peer in peers {
+        info.push(format!("{}->|{:?},{:?}|", peer.0.to_base58(), peer.1.bound_type, peer.1.connect_time));
+    }
+    info.join(",")
 }
 
 impl Actor for TcpServer {
@@ -51,15 +87,12 @@ impl Actor for TcpServer {
     fn started(&mut self, ctx: &mut Self::Context) {
         info!("[{:?}] Server start, listen on: {:?}", self.node_info.0, self.node_info.1);
         ctx.run_interval(::std::time::Duration::from_secs(2), move |act, ctx| {
-            let peers: String = act.peers.keys().map(|key| key.to_base58()).collect::<Vec<String>>().join(",");
-            info!("Connect clients: {}, [{}]", act.peers.len(), peers);
-            let dia_peers: String = act.dial_peers.keys().map(|key| key.to_base58()).collect::<Vec<String>>().join(",");
-            info!("Dialing clients: {}, [{}]", act.dial_peers.len(), dia_peers);
+            info!("Connect clients: {},local-id:{}, [{}]", act.peers.len(), act.node_info.0.to_base58(), node_info(&act.peers));
         });
     }
 
     fn stopped(&mut self, ctx: &mut Self::Context) {
-        info!("[{:?}] Server stopped, listen on: {:?}", self.node_info.0, self.node_info.1);
+        info!("[{:?}] Server stopped, listen on: {:?}", self.node_info.0.to_base58(), self.node_info.1);
     }
 }
 
@@ -81,31 +114,28 @@ impl Handler<P2PEvent> for TcpServer {
 }
 
 impl Handler<ServerEvent> for TcpServer {
-    type Result = ();
+    type Result = Result<PeerId, P2PError>;
     fn handle(&mut self, msg: ServerEvent, ctx: &mut Self::Context) -> Self::Result {
         match msg {
-            ServerEvent::Connected(ref peer_id) => {
+            ServerEvent::Connected(ref peer_id, ref bound_type, ref raw_msg) => {
                 trace!("Connected peer: {:?}", peer_id);
-                if self.peers.contains_key(peer_id) {
-                    unimplemented!("It should be happen");
-                    return ();
-                }
-                self.peers.entry(peer_id.clone()).or_insert(0);
-                self.dial_peers.remove(peer_id);
+                self.handle_handshake(bound_type.clone(), raw_msg.payload())?;
             }
             ServerEvent::Disconnected(ref peer_id) => {
                 trace!("Disconnected peer: {:?}", peer_id);
                 self.peers.remove(&peer_id);
-                self.dial_peers.remove(peer_id);
+            }
+            ServerEvent::Message(ref raw_msg) => {
+//                self.handle_network_message(raw_msg.clone())?;
             }
         }
-        ()
+        Ok(ZERO_PEER.clone())
     }
 }
 
 impl TcpServer {
     pub fn new(peer_id: PeerId, mul_addr: Multiaddr, key: Option<secio::SecioKeyPair>) -> Addr<TcpServer> {
-        let mut addr: String = "".to_string();
+        let mut addr: String = String::new();
         mul_addr.iter().for_each(|item| match &item {
             Protocol::Ip4(ref ip4) => {
                 addr.push_str(&format!("{}:", ip4));
@@ -127,9 +157,8 @@ impl TcpServer {
                 lis.incoming()
                     .map_err(|_| ())
                     .map(move |s| {
-                        let peer_id_c = peer_id_c.clone();
-                        // There have a problem
-                        TcpConnect(s, peer_id_c)
+                        TcpConnectInBound(s)
+                        // start new connection
                     }),
             );
             TcpServer {
@@ -137,60 +166,106 @@ impl TcpServer {
                 key: key,
                 node_info: (peer_id.clone(), mul_addr.clone()),
                 peers: HashMap::new(),
-                dial_peers: HashMap::new(),
             }
         })
     }
 
     fn add_peer(&mut self, remote_id: PeerId, remote_addresses: Vec<Multiaddr>) {
-        let id = Uuid::new_v5(&Uuid::NAMESPACE_DNS, chrono::Local::now().to_string().as_bytes());
-        if self.dial_peers.contains_key(&remote_id) || self.peers.contains_key(&remote_id) {
+        let id = random_uuid();
+        if self.peers.contains_key(&remote_id) {
             return;
         }
 
-        let result = self.dial_peers.entry(remote_id.clone()).or_insert(id.clone());
-        assert_eq!(*result, id);
-
         // try to connect, dial it
-        TcpDial::new(remote_id, remote_addresses[0].clone(), self.pid.clone());
+        TcpDial::new(remote_id, self.node_info.0.clone(), remote_addresses[0].clone(), self.pid.clone());
     }
 
     // TODO
     fn drop_peer(&mut self, remote_id: PeerId, remote_addresses: Vec<Multiaddr>) {}
+
+    fn handle_network_message(&mut self, msg: RawMessage) -> Result<(), P2PError> {
+        let header = msg.header();
+        match header.code {
+            P2PMsgCode::Handshake => {
+                return Err(P2PError::InvalidMessage);
+            }
+            _ => {
+                unimplemented!()
+            }
+        }
+
+        Ok(())
+    }
+
+    fn handle_handshake(&mut self, bound_type: BoundType, payload: &Vec<u8>) -> Result<PeerId, P2PError> {
+        use std::borrow::Cow;
+        let handshake: Handshake = Handshake::from_bytes(Cow::from(payload));
+        info!("-----> {:?}", handshake);
+        let peer_id = handshake.peer_id();
+        if self.peers.contains_key(&peer_id) {
+            return Err(P2PError::DumpConnected);
+        }
+        if self.node_info.0 == handshake.peer_id() {
+            return Err(P2PError::HandShakeFailed);
+        }
+        let connect_info = ConnectInfo::new(chrono::Utc::now(), BoundType::InBound);
+        self.peers.entry(peer_id.clone()).or_insert(connect_info);
+        Ok(peer_id)
+    }
 }
 
 #[derive(Message)]
-struct TcpConnect(TcpStream, PeerId);
+struct TcpConnectOutBound(TcpStream, PeerId);
 
 /// Handle stream of TcpStream's
-impl Handler<TcpConnect> for TcpServer {
+impl Handler<TcpConnectOutBound> for TcpServer {
     type Result = ();
 
-    fn handle(&mut self, msg: TcpConnect, ctx: &mut Context<Self>) {
+    fn handle(&mut self, msg: TcpConnectOutBound, ctx: &mut Context<Self>) {
         trace!("TcpServer receive tcp connect event, peerid: {:?}", msg.1);
         // For each incoming connection we create `session` actor with out chat server
-        if self.dial_peers.contains_key(&msg.1) {
-            msg.0.shutdown(net::Shutdown::Both);
-            return;
-        }
         if self.peers.contains_key(&msg.1) {
             msg.0.shutdown(net::Shutdown::Both);
             return;
         }
 
-        let id = Uuid::new_v5(&Uuid::NAMESPACE_DNS, chrono::Local::now().to_string().as_bytes());
-        let got_id = self.dial_peers.entry(msg.1.clone()).or_insert(id.clone());
-        assert_eq!(*got_id, id);
-
         let peer_id = msg.1.clone();
         let server_id = self.pid.clone();
+        let local_id = self.node_info.0.clone();
         Session::create(|ctx| {
             let (r, w) = msg.0.split();
             Session::add_stream(FramedRead::new(r, MsgPacketCodec), ctx);
             Session::new(
+                ctx.address().clone(),
                 peer_id,
+                local_id,
                 server_id,
                 actix::io::FramedWrite::new(w, MsgPacketCodec, ctx),
+                BoundType::OutBound,
+            )
+        });
+    }
+}
+
+#[derive(Message)]
+struct TcpConnectInBound(TcpStream);
+
+impl Handler<TcpConnectInBound> for TcpServer {
+    type Result = ();
+
+    fn handle(&mut self, msg: TcpConnectInBound, ctx: &mut Context<Self>) {
+        let server_id = self.pid.clone();
+        let local_id = self.node_info.0.clone();
+        Session::create(|ctx| {
+            let (r, w) = msg.0.split();
+            Session::add_stream(FramedRead::new(r, MsgPacketCodec), ctx);
+            Session::new(
+                ctx.address().clone(),
+                ZERO_PEER.clone(),
+                local_id,
+                server_id,
+                actix::io::FramedWrite::new(w, MsgPacketCodec, ctx),
+                BoundType::InBound,
             )
         });
     }
@@ -205,17 +280,23 @@ impl Actor for TcpDial {
 }
 
 impl TcpDial {
-    pub fn new(peer_id: PeerId, mul_addr: Multiaddr, tcp_server: Addr<TcpServer>) {
+    pub fn new(peer_id: PeerId, local_id: PeerId, mul_addr: Multiaddr, tcp_server: Addr<TcpServer>) {
         let socket_addr = multiaddr_to_ipv4(&mul_addr).unwrap();
         trace!("Try to dial remote peer, peer_id:{:?}, network: {:?}", &peer_id, &socket_addr);
         Arbiter::spawn(TcpStream::connect(&socket_addr).and_then(move |stream| {
             trace!("Dialing remote peer: {:?}", peer_id);
             let peer_id = peer_id.clone();
+            let local_id = local_id.clone();
             let tcp_server = tcp_server.clone();
             Session::create(|ctx| {
                 let (r, w) = stream.split();
                 Session::add_stream(FramedRead::new(r, MsgPacketCodec), ctx);
-                Session::new(peer_id, tcp_server, actix::io::FramedWrite::new(w, MsgPacketCodec, ctx))
+                Session::new(ctx.address().clone(),
+                             peer_id,
+                             local_id,
+                             tcp_server,
+                             actix::io::FramedWrite::new(w, MsgPacketCodec, ctx),
+                             BoundType::OutBound)
             });
 
             futures::future::ok(())
